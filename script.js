ed1bc0a248b13531eff32d9b0648d371adaf50b5
ivanov <ivanov@mail.com>
2025-09-25 15:13:45 +0300

    changeText

---
+++ script.js
@@ -0,0 +1,16 @@
+let n1,n2, число1;
+n1 = confirm("Are you sure?")
+//Однострочный
+/*
+asdasd
+asdsad
+*/
+alert(n1)
+s='12332'
+s1="asdsad"
+s3=`asasd ${s} asdasdasddsa`
+let i=0;
+let n=null
+let q;
+
+alert('abc'*3)
